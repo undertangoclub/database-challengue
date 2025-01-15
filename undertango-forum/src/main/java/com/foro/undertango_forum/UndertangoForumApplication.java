0aa174dfda748a4c8444005eab1132b3f448ec17
undertangoclub <pablocieslik@gmail.com>
2025-01-15 01:12:36 -0300

    Inicializando el proyecto foro undertango

---
+++ undertango-forum/src/main/java/com/foro/undertango_forum/UndertangoForumApplication.java
@@ -0,0 +1,13 @@
+package com.foro.undertango_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndertangoForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndertangoForumApplication.class, args);
+	}
+
+}
